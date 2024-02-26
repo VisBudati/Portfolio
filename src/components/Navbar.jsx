f2dc6c91a3c049f69aeec646d162200654c7e8e1
VisBudati <123014917+VisBudati@users.noreply.github.com>
2024-02-26 13:58:44 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
-import VisitorCounter from "./canvas/visitorcounter";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -31,8 +30,6 @@ const Navbar = () => {
           <span className="sm:block hidden">|  Data Scientist</span>
           </p>
         </Link>
-
-        <VisitorCounter/>
 
         <ul className="list-none hidden sm:flex 
         flex-row gap-10">
